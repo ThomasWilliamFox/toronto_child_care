d1b1cc07b34a4daee0dae8a31cb45d296f6278d9
ThomasWilliamFox <thomas.fox@mail.utoronto.ca>
2024-01-21 21:16:06 -0500

    - Child care, census info (population, income), and ward names saved to data outputs - Data cleaning complete

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -1,44 +1,122 @@
 #### Preamble ####
-# Purpose: Cleans the raw plane data recorded by two observers..... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 6 April 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Cleans raw Toronto child care and census data
+# Author: Thomas Fox
+# Date: 21 January 2024
+# Contact: thomas.fox@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: n/a
+# Any other information needed? n/a
 
 #### Workspace setup ####
 library(tidyverse)
+library(dplyr)
+library(janitor)
 
 #### Clean data ####
-raw_data <- read_csv("inputs/data/plane_data.csv")
 
-cleaned_data <-
-  raw_data |>
-  janitor::clean_names() |>
-  select(wing_width_mm, wing_length_mm, flying_time_sec_first_timer) |>
-  filter(wing_width_mm != "caw") |>
+# Read childcare data
+
+childcare_data <- read_csv("inputs/data/raw_childcare_data.csv")
+names(childcare_data)
+
+# Clean child care data, all names to lower case
+# Select center id, ward number, total spaces, and type(non-profit/commercial)
+cleaned_childcare_data <- 
+  clean_names(data) |>
+  select(x_id, ward, totspace, auspice) |>
   mutate(
-    flying_time_sec_first_timer = if_else(flying_time_sec_first_timer == "1,35",
-                                   "1.35",
-                                   flying_time_sec_first_timer)
-  ) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "490",
-                                 "49",
-                                 wing_width_mm)) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "6",
-                                 "60",
-                                 wing_width_mm)) |>
+    ward = as.character(ward)
+  )
+
+cleaned_childcare_data
+
+
+# Read and clean ward name data 
+
+ward_name_data <- read_csv("inputs/data/raw_ward_names.csv")
+
+ward_name_data <- 
+  clean_names(ward_name_data)
+
+ward_name_data
+
+
+# Read and clean 2021 Canada census data
+
+census_data <- read_csv("inputs/data/raw_census_data.csv")
+
+# Get subset of data covering total population and population under 14 by ward 
+population_data <- census_data[c(18:21),c(1:27)]
+
+population_data
+
+# Get subset of data covering average/total/and median household incomes by ward
+income_data <- census_data[c(1383:1384),c(1:27)]
+income_data
+
+# Merges income and population subsets together 
+census_data_merged <- rbind(population_data, income_data)
+census_data_merged
+
+# Transposes x and y axis  
+cleaned_census_data_temp <- t(census_data_merged)
+
+# Turns matrix from transposing into data frame 
+cleaned_census_df <- 
+  as.data.frame(cleaned_census_data_temp)
+
+# Converts data frame to tibble 
+cleaned_census_data <- tibble(cleaned_census_df)
+cleaned_census_data
+
+# Uses first row as names of variables 
+cleaned_census_data <- cleaned_census_data |>
+  row_to_names(row_number = 1)
+
+# Adds column to indicate wards
+cleaned_census_data <- 
+  cleaned_census_data |> add_column(ward = 0:25, .before = "Total - Age")
+
+# Renames first ward "0" to Toronto (City total)
+cleaned_census_data <-
+  cleaned_census_data |>
   mutate(
-    wing_width_mm = as.numeric(wing_width_mm),
-    wing_length_mm = as.numeric(wing_length_mm),
-    flying_time_sec_first_timer = as.numeric(flying_time_sec_first_timer)
-  ) |>
-  rename(flying_time = flying_time_sec_first_timer,
-         width = wing_width_mm,
-         length = wing_length_mm
-         ) |> 
-  tidyr::drop_na()
+    ward = as.character(ward)
+  ) 
+cleaned_census_data[cleaned_census_data == 0] <- "Toronto"
+
+# Rename variable names 
+cleaned_census_data <-
+  cleaned_census_data |>
+  rename(pop_total = `Total - Age`,
+         pop_0_to_4 = `0 to 4 years`,
+         pop_5_to_9 = `5 to 9 years`,
+         pop_10_to_14 = `10 to 14 years`,
+         avg_hh_income = `Average total income of households in 2020 ($)`,
+         med_hh_income = `Median total income of households in 2020 ($)`
+         )
+
+# Convert all numerical columns to int or num 
+cleaned_census_data <-
+  cleaned_census_data |>
+  mutate(
+    pop_total = as.integer(pop_total),
+    pop_0_to_4 = as.integer(pop_0_to_4),
+    pop_5_to_9 = as.integer(pop_5_to_9),
+    pop_10_to_14 = as.integer(pop_10_to_14),
+    avg_hh_income = as.numeric(avg_hh_income),
+    med_hh_income = as.numeric(med_hh_income)
+  ) 
+
 
 #### Save data ####
-write_csv(cleaned_data, "outputs/data/analysis_data.csv")
+
+# Save cleaned census data 
+write_csv(cleaned_census_data, "outputs/data/census_data.csv")
+
+# Save cleaned ward name data 
+write_csv(ward_name_data, "outputs/data/ward_names.csv")
+
+# Save cleaned child care data 
+write_csv(cleaned_childcare_data, "outputs/data/child_care_data.csv")
+
